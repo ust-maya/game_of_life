146d8657c801951ca601da2243d9dec8b90afddc
Igor Popov <igpd@bk.ru>
2022-08-27 06:20:53 +0700

    add preset file, include ncurses, add controls

---
+++ src/game_of_life.c
@@ -0,0 +1,34 @@
+#include <ncurses.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "field.h"
+
+int main() {
+    char field[HEIGHT_X_25][WIDTH_Y_80];
+    char field_next_generation[HEIGHT_X_25][WIDTH_Y_80];
+    int ch = 0;
+    int speed = 100000;
+    initField(field, field_next_generation);
+    initFirstGeneration(field);
+    initscr();
+    keypad(stdscr, TRUE);
+    raw();
+    noecho();
+    nodelay(stdscr, TRUE);
+    curs_set(0);
+    while (ch != 'q') {
+        ch = getch();
+        if (ch == 'w') { speed -= 10000; }
+        if (ch == 's') { speed += 10000; }
+        if (speed < 10000) { speed = 10000; }
+        usleep(speed);
+        erase();
+        aliveOrDead(field, field_next_generation);
+        updateField(field, field_next_generation);
+        renderGeneration(field);
+    }
+    curs_set(1);
+    endwin();
+    return 0;
+}
