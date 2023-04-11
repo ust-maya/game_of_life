7dcaa9735437de09a2e8ddd05b0e99cac50223bf
Rebeccaa <rebeccaa@student.21-school.ru>
2023-04-11 22:39:30 +0700

    codestyle update + add new func runGame

--- src/field.h
+++ src/field.h
@@ -1,19 +1,20 @@
 #ifndef FIELD_H
 #define FIELD_H
 
+#include <ncurses.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #define ALIVE 'O'
 #define DEAD '.'
 #define HEIGHT_X_25 25
 #define WIDTH_Y_80 80
 
-void initField(char field[][WIDTH_Y_80],
-               char field_next_generation[][WIDTH_Y_80]);
-void renderGeneration(char field[][WIDTH_Y_80]);
+void initField(char field[][WIDTH_Y_80], char field_next_generation[][WIDTH_Y_80]);
+void renderGeneration(char field[][WIDTH_Y_80], int speed);
 void initFirstGeneration(char field[][WIDTH_Y_80]);
 int countNeighbors(char field[][WIDTH_Y_80], int x, int y);
-void aliveOrDead(char field[][WIDTH_Y_80],
-                 char field_next_generation[][WIDTH_Y_80]);
-void updateField(char field[][WIDTH_Y_80],
-                 char field_next_generation[][WIDTH_Y_80]);
+void aliveOrDead(char field[][WIDTH_Y_80], char field_next_generation[][WIDTH_Y_80]);
+void updateField(char field[][WIDTH_Y_80], char field_next_generation[][WIDTH_Y_80]);
 
-#endif//FIELD_H
+#endif  // FIELD_H
